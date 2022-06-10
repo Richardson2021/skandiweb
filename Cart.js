109214f4a53300aab5849528342cf1d7754cf0fe
Richardson2021 <96339724+Richardson2021@users.noreply.github.com>
2022-06-10 18:40:14 +0300

    Update Cart.js

--- Cart.js
+++ Cart.js
@@ -8,22 +8,6 @@ export default class Cart extends Component {
           {cartItems.length === 0? <div className="cart cart-header">Cart is empty</div>
           :
           <div className="cart cart-header">You have {cartItems.length} in the cart{" "}</div>}
-          <div className="cart">
-            <ul className="cartItems">{Cart.Items.map(item=>(
-              <li key="Items._id">
-                <div>
-                  <img src={item.image} alt={item.title}></img>
-                </div>
-                <div>
-                  <div>{cartItems.title}</div>
-                  <div className="right">
-                    {(item.price)} x {item.count}{" "}
-                    <button className="button" onClick={()=>this.props.removeFromCart(item)}>Remove</button></div>
-                </div>
-              </li>
-            ))}</ul>
-            </div>
-            
       </div>
       
       
@@ -34,6 +18,21 @@ export default class Cart extends Component {
 
   }
 }
+
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
 
   
       
